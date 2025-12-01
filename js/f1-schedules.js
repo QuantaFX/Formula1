a33e9c769dec5b5f2d4d28d251444199d01ffe6e
Sheape <paulpare@protonmail.com>
2025-12-01 19:25:13 +0800

    feat: add styling for meetings schedule

--- js/f1-schedules.js
+++ js/f1-schedules.js
@@ -7,12 +7,28 @@ export async function loadSchedule() {
 	const parsedData = await response.json();
 
 	const container = document.getElementById("f1-schedule-list");
-	parsedData.forEach((data) => {
-		const newDiv = document.createElement("div");
-		newDiv.classList.add("bg-primary");
-		newDiv.textContent = data.circuit_short_name;
+	parsedData.forEach((data, i, _) => {
+		const parent = document.createElement("div");
+		parent.classList.add(
+			"schedule-main-item",
+			"list-group-item",
+			"list-group-item-action",
+			"rounded-3",
+		);
+		parent.innerHTML = `
+            <div class="d-flex flex-row justify-content-between">
+                <div class="schedule-main-subcaption text-white-50">ROUND ${i + 1}</div>
+                <span class="schedule-main-subcaption text-white-50 px-3 bg-dark rounded-pill">
+                    14-16 MAR
+                </span>
+            </div>
+            <div class="schedule-main-caption text-white fs-3 fw-bold">${data.country_name}</div>
+            <span class="schedule-main-desc text-white-50 fw-semibold">
+                ${data.meeting_official_name}
+            </span>
+        `;
 
-		container.appendChild(newDiv);
+		container.appendChild(parent);
 	});
 }
 
